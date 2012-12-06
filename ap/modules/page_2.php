b826ff5f9e9bf8f60ea0bcf8445fd42b835f8e16
Aliaksandr Pankou <il_mondo@inbox.ru>
2012-12-06 11:18:20 +0300

    Revert "add project files"
    This reverts commit d0c54fe956e58d512913264eba0adb0d8b8af7fb.

--- ap/modules/page_2.php
+++
@@ -1,9 +0,0 @@
-<div><form action="components/add.php" method="post">
-<table>
-<tr><td>ID: </td><td><input type="text" name="id" placeholder="ID here"></td></tr>
-<tr><td>TITLE: </td><td><input type="text" name="title" placeholder="Title here"></td></tr>
-<tr><td>ARTICLE:</td><td>
-<textarea cols="40" rows="10" name="text" placeholder="Article text here"></textarea></td></tr>
-</table>
-<input type="submit" name="post" value="Add">
-</form></div>
