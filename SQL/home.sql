b581de494ea26a3ee7430206618c7b53502bc166
Aliaksandr Pankou <il_mondo@inbox.ru>
2012-12-07 12:58:01 +0300

    SQL UPDATE

--- SQL/home.sql
+++ SQL/home.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Дек 07 2012 г., 13:48
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -7,14 +16,27 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- База данных: `home`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `content`
+--
 
 CREATE TABLE IF NOT EXISTS `content` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `title` text,
   `text` text,
   `data` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=68 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Дамп данных таблицы `content`
+--
 
 INSERT INTO `content` (`id`, `title`, `text`, `data`) VALUES
 (1, 'Article #1', 'The internet has changed a lot since HTML 4.01 became a standard in 1999.</ br>\r\n\r\nToday, some elements in HTML 4.01 are obsolete, never used, or not used the way they were intended to. These elements are removed or re-written in HTML5.\r\n\r\nTo better handle today''s internet use, HTML5 includes new elements for better structure, better form handling, drawing, and for media content.', '0'),
@@ -22,7 +44,11 @@ INSERT INTO `content` (`id`, `title`, `text`, `data`) VALUES
 (3, 'Article #3', 'HTML5 is a cooperation between the World Wide Web Consortium (W3C) and the Web Hypertext Application Technology Working Group (WHATWG).\r\n\r\nWHATWG was working with web forms and applications, and W3C was working with XHTML 2.0. In 2006, they decided to cooperate and create a new version of HTML.', '0'),
 (4, 'Article #4', 'HTML5 is not yet an official standard, and no browsers have full HTML5 support.\r\n\r\nBut all major browsers (Safari, Chrome, Firefox, Opera, Internet Explorer) continue to add new HTML5 features to their latest versions.\r\n', '04 Dec  17:12:12');
 
+-- --------------------------------------------------------
 
+--
+-- Структура таблицы `settings`
+--
 
 CREATE TABLE IF NOT EXISTS `settings` (
   `title` varchar(32) NOT NULL,
@@ -32,22 +58,27 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `id` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `settings`
+--
 
 INSERT INTO `settings` (`title`, `owner`, `description`, `keywords`, `id`) VALUES
-('Unengine', 'admin', 'Unengine- my php solution!', 'unengine, test, php, branch', 1);
+('Unengine', 'admin', 'Unengine- my php solution!', 'unengine, php, engine', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usr` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `pass` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `regIP` varchar(15) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `dt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `permissions` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `usr` (`usr`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `username` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `perm` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
