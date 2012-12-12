4eabcfea7dd5535ac677bde51c701a05611d9cd6
Aliaksandr Pankou <il_mondo@inbox.ru>
2012-12-12 17:32:25 +0300

    modules up
    add short & full article textarea;
    add comment form in full article page.

--- ap/modules/page_2.php
+++ ap/modules/page_2.php
@@ -1,9 +1,11 @@
 <div><form action="components/add.php" target="ajax" method="post">
 <table>
-<tr><td>ID: </td><td><input type="text" name="id" placeholder="ID here"></td></tr>
+<!--<tr><td>ID: </td><td><input type="text" name="id" placeholder="ID here"></td></tr>!-->
 <tr><td>TITLE: </td><td><input type="text" name="title" placeholder="Title here"></td></tr>
-<tr><td>ARTICLE:</td><td>
-<textarea cols="40" rows="10" name="text" placeholder="Article text here"></textarea></td></tr>
+<tr><td>SHORT:</td><td>
+<textarea cols="40" rows="5" name="short" placeholder=" Short Article text here"></textarea></td></tr>
+<tr><td>FULL:</td><td>
+<textarea cols="40" rows="10" name="full" placeholder="Full Article text here"></textarea></td></tr>
 </table>
 <input type="submit" name="post" value="Add">
 </form>
