c377575dfc7678c48fd962684e885210798a94c9
Aliaksandr Pankou <il_mondo@inbox.ru>
2012-12-26 12:00:44 +0300

    update sql & core files

--- core/data/submit.php
+++ core/data/submit.php
@@ -7,10 +7,10 @@ $email = htmlspecialchars ($email);
 $text = trim ($_POST["text"]);
 $text = htmlspecialchars ($text);
 $date = date("d M  Y G:i:s");
-$uid = $_POST["uid"];
+$rule = $_POST["rule"];
 if (!isset($email) or empty ($email)) {echo "<meta http-equiv='refresh' content='3;URL=$_SERVER[HTTP_REFERER]'>";exit ("<div align='center'><div class='alert'>Please enter email!</div></div>");}
 if (!isset($text) or empty ($text)) {echo "<meta http-equiv='refresh' content='3;URL=$_SERVER[HTTP_REFERER]'>";exit ("<div align='center'><div class='alert'>Please enter comment text!</div></div>");}
-$insert = mysql_query ("INSERT INTO ".$tbl." (name,date,email,text,uid) VALUES ('$name','$date','$email','$text','$uid')",$link);
+$insert = mysql_query ("INSERT INTO ".$tbl." (rule,name,date,email,text) VALUES ('$rule','$name','$date','$email','$text')",$link);
 if($insert == "true") {echo "<meta http-equiv='refresh' content='3;URL=$_SERVER[HTTP_REFERER]'>";echo "<div align='center'><div class='alert'>Success!</div></div>";}
 else {echo "<meta http-equiv='refresh' content='3;URL=$_SERVER[HTTP_REFERER]'>";echo "<div align='center'><div class='alert'>Error!</div></div>";}
 ?>
--- sql/home.sql
+++ sql/home.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Дек 19 2012 г., 14:13
+-- Время создания: Дек 26 2012 г., 12:54
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -27,21 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `comment` (
-  `id` int(99) NOT NULL AUTO_INCREMENT,
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `rule` int(9) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `text` text NOT NULL,
-  `uid` int(11) DEFAULT NULL,
   `date` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=cp1251 AUTO_INCREMENT=13 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 --
 -- Дамп данных таблицы `comment`
 --
 
-INSERT INTO `comment` (`id`, `name`, `email`, `text`, `uid`, `date`) VALUES
-(12, 'admin', 'aliaksandr_pankou@epam.com', 'Test comments', 5, '13 Dec  2012 18:52:05');
+INSERT INTO `comment` (`id`, `rule`, `name`, `email`, `text`, `date`) VALUES
+(12, 5, 'admin', 'aliaksandr_pankou@epam.com', 'Test comments', '13 Dec  2012 18:52:05');
 
 -- --------------------------------------------------------
 
@@ -50,24 +50,24 @@ INSERT INTO `comment` (`id`, `name`, `email`, `text`, `uid`, `date`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `content` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(9) NOT NULL AUTO_INCREMENT,
   `title` text,
   `short_article` text,
   `full_article` text NOT NULL,
   `data` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Дамп данных таблицы `content`
 --
 
 INSERT INTO `content` (`id`, `title`, `short_article`, `full_article`, `data`) VALUES
-(1, 'Article #1', 'The internet has changed a lot since HTML 4.01 became a standard in 1999.</ br>\r\n\r\nToday, some elements in HTML 4.01 are obsolete, never used, or not used the way they were intended to.', ' These elements are removed or re-written in HTML5.\r\n\r\nTo better handle today''s internet use, HTML5 includes new elements for better structure, better form handling, drawing, and for media content.', '0'),
-(2, 'Article #2', 'HTML5 will be the new standard for HTML.\r\n\r\nThe previous version of HTML, HTML 4.01, came in 1999. The web has changed a lot since then.\r\n\r\n', 'HTML5 is still a work in progress. However, the major browsers support many of the new HTML5 elements and APIs.', '0'),
-(3, 'Article #3', 'HTML5 is a cooperation between the World Wide Web Consortium (W3C) and the Web Hypertext Application Technology Working Group (WHATWG).\r\n', 'WHATWG was working with web forms and applications, and W3C was working with XHTML 2.0. In 2006, they decided to cooperate and create a new version of HTML.', '0'),
-(4, 'Article #4', 'HTML5 is not yet an official standard, and no browsers have full HTML5 support.\r\n\r\nBut all major browsers (Safari, Chrome, Firefox, Opera, Internet Explorer) continue to add new HTML5 features to their latest versions.\r\n', 'But all major browsers (Safari, Chrome, Firefox, Opera, Internet Explorer) continue to add new HTML5 features to their latest versions.', '04 Dec  17:12:12'),
-(5, 'Welcome!', 'Welcome to short article. Click Read Me to go from Full article.', 'Hi! This is full article text!', '12 Dec 17:12:10');
+(1, 'Article #1', 'The internet has changed a lot since HTML 4.01 became a standard in 1999.</ br>\r\n\r\nToday, some elements in HTML 4.01 are obsolete, never used, or not used the way they were intended to.', ' These elements are removed or re-written in HTML5.\r\n\r\nTo better handle today''s internet use, HTML5 includes new elements for better structure, better form handling, drawing, and for media content.', NULL),
+(2, 'Article #2', 'HTML5 will be the new standard for HTML.\r\n\r\nThe previous version of HTML, HTML 4.01, came in 1999. The web has changed a lot since then.\r\n\r\n', 'HTML5 is still a work in progress. However, the major browsers support many of the new HTML5 elements and APIs.', NULL),
+(3, 'Article #3', 'HTML5 is a cooperation between the World Wide Web Consortium (W3C) and the Web Hypertext Application Technology Working Group (WHATWG).\r\n', 'WHATWG was working with web forms and applications, and W3C was working with XHTML 2.0. In 2006, they decided to cooperate and create a new version of HTML.', NULL),
+(4, 'Article #4', 'HTML5 is not yet an official standard, and no browsers have full HTML5 support.\r\n\r\nBut all major browsers (Safari, Chrome, Firefox, Opera, Internet Explorer) continue to add new HTML5 features to their latest versions.\r\n', 'But all major browsers (Safari, Chrome, Firefox, Opera, Internet Explorer) continue to add new HTML5 features to their latest versions.', NULL),
+(5, 'Welcome!', 'Welcome to short article. Click Read Me to go from Full article.', 'Hi! This is full article text!', NULL);
 
 -- --------------------------------------------------------
 
@@ -76,19 +76,20 @@ INSERT INTO `content` (`id`, `title`, `short_article`, `full_article`, `data`) V
 --
 
 CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(9) NOT NULL,
   `title` varchar(32) NOT NULL,
   `owner` varchar(15) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `keywords` varchar(255) DEFAULT NULL,
-  `id` int(9) NOT NULL
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `settings`
 --
 
-INSERT INTO `settings` (`title`, `owner`, `description`, `keywords`, `id`) VALUES
-('Unengine', 'admin', 'Unengine- my php solution!', 'unengine, php, engine', 1);
+INSERT INTO `settings` ( `id`,`title`, `owner`, `description`, `keywords`) VALUES
+(1,'Unengine', 'admin', 'Unengine- my php solution!', 'unengine, php, engine');
 
 -- --------------------------------------------------------
 
@@ -97,11 +98,11 @@ INSERT INTO `settings` (`title`, `owner`, `description`, `keywords`, `id`) VALUE
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `username` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `permission` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `permission` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
 
